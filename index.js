e4fe6dadcd4de4808c3aa1b27e603251c547d3a8
Adam Kabak <adamkabak@yahoo.com>
2022-09-18 17:11:44 -0400

    commit tree ebf8e33c67436cb21ee411e9731c25206b4fbd80

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ft){
+  if (ft <= 400) return 'This one is on me!';
+  if (ft > 400 && ft <= 2000) return 'That will be twenty bucks.';
+  if (ft > 2500) return 'No can do.';
+  if (ft > 2000) return 'I will gladly take your thirty bucks.'; 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') return 'Ok, sounds good.';
+  return 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') return 'Thank you so much.'
+  if (tip === 'not as generous') return 'Thank you.'
+  return 'Bye.'
 }
